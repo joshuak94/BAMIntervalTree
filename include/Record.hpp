e85c414ae38edbc0e29e682a3f919f3fb5b7f2ae
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-03-24 16:13:59 +0100

    [FEATURE] Add comparators for Records.

--- include/Record.hpp
+++ include/Record.hpp
@@ -22,7 +22,7 @@ struct RecordComparatorEnd
     bool operator ()(const Record & record1, const Record & record2)
     {
         if(record1.end== record2.end)
-            return record1.start < record2.start;
-        return record1.end < record2.end;
+            return record1.start > record2.start;
+        return record1.end > record2.end;
     }
 };
