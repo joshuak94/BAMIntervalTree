7dbf805215b62fa9998cdd4fdfdd6e5174d165a3
Josh Kim <joshuak94@zedat.fu-berlin.de>
2021-03-24 16:17:07 +0100

    Update test files to add multiple chromosomes. Also keep track of cumulative chromosome length.

--- src/main.cpp
+++ src/main.cpp
@@ -58,24 +58,28 @@ int main(int argc, char ** argv)
     seqan3::sam_file_input input{options.input_path, my_fields{}};
 
     seqan3::debug_stream << "Extracting info from reads\n";
+
     // Calculate total length of genome.
-    // size_t length{0};
-    // for (auto c : input.header().ref_id_info)
-    // {
-    //     length += std::get<0>(c);
-    // }
+    size_t running_sum{0};
+    std::vector<size_t> cumulative_length{};
+    for (auto c : input.header().ref_id_info)
+    {
+        cumulative_length.push_back(running_sum);
+        running_sum += std::get<0>(c);
+    }
+
     std::vector<Record> records{};
     for (auto & r : input)
     {
-        int32_t start = std::get<2>(r).value();
-        int32_t end = std::get<2>(r).value() + std::get<4>(r).size();
+        int32_t ref_id = std::get<1>(r).value();
+        int32_t start = std::get<2>(r).value() + cumulative_length[ref_id];
+        int32_t end = std::get<2>(r).value() + std::get<4>(r).size() + cumulative_length[ref_id];
         Record rec{start, end};
         records.push_back(rec);
     }
 
     seqan3::debug_stream << "Creating Node.\n";
     std::unique_ptr<IntervalNode> root(nullptr);
-
 
     construct_tree(root, records);
 
