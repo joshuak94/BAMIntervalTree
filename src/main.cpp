033c85fbff73ce8b46da2f83ad51dfa5ac3d98d6
Josh Kim <joshuak94@zedat.fu-berlin.de>
2021-03-30 16:57:35 +0200

    Update separator for searching to a comma and update documentation.

--- src/main.cpp
+++ src/main.cpp
@@ -10,32 +10,39 @@ struct CmdOptions
     std::string end{};
 };
 
+/*!
+   \brief Parse the start and end strings from the given options and put them into the given bamit::Position containers.
+   \param start Where the resulting start bamit::Position will go.
+   \param end Where the resulting end bamit::Position will go.
+   \param options The CmdOptions object storing the start and end strings from the user.
+   \return 0 if the parsing was successful, -1 otherwise.
+*/
 int32_t const parse_overlap_query(bamit::Position & start,
                                   bamit::Position & end,
                                   CmdOptions const & options,
                                   std::deque<std::string> const & ref_ids)
 {
-    size_t const start_split = options.start.find(':');
-    size_t const end_split = options.end.find(':');
+    // Get the location of the colon in the start and end.
+    size_t const start_split = options.start.find(',');
+    size_t const end_split = options.end.find(',');
     if (start_split == options.start.size() || end_split == options.start.size())
     {
         seqan3::debug_stream << "[ERROR] Start and end positions must be in the format chr_name:position!\n";
         return -1;
     }
-    auto start_ref_id_it = std::find(ref_ids.begin(),
-                                     ref_ids.end(),
-                                     options.start.substr(0, start_split));
-    auto end_ref_id_it = std::find(ref_ids.begin(),
-                                ref_ids.end(),
-                                options.end.substr(0, end_split));
 
-    int32_t start_position{};
-    int32_t end_position{};
+    // Get the iterator position of the chromosomes given.
+    auto start_ref_id_it = std::find(ref_ids.begin(), ref_ids.end(), options.start.substr(0, start_split));
+    auto end_ref_id_it = std::find(ref_ids.begin(), ref_ids.end(), options.end.substr(0, end_split));
     if (start_ref_id_it == ref_ids.end() || end_ref_id_it == ref_ids.end())
     {
         seqan3::debug_stream << "[ERROR] One or both of the chromosome names supplied could not be found.\n";
         return -1;
     }
+
+    // Convert the start and end strings to integers.
+    int32_t start_position{};
+    int32_t end_position{};
     try
     {
         start_position = std::stoi(options.start.substr(start_split + 1));
@@ -46,6 +53,7 @@ int32_t const parse_overlap_query(bamit::Position & start,
         seqan3::debug_stream << "[ERROR] There was a formatting error with the overlap query!\n";
         return -1;
     }
+
     // Convert query string into two Positions.
     int32_t start_ref_id = start_ref_id_it - ref_ids.begin();
     int32_t end_ref_id = end_ref_id_it - ref_ids.begin();
@@ -69,18 +77,18 @@ void initialize_argument_parser(seqan3::argument_parser & parser, CmdOptions & o
 
     // Ref ID regex taken from the SAM format manual.
     std::string ref_id_match{"[0-9A-Za-z!#$%&+./:;?@^_|~-][0-9A-Za-z!#$%&*+./:;=?@^_|~-]*"};
-    seqan3::regex_validator query_validator{ref_id_match + ":[0-9]+"};
+    seqan3::regex_validator query_validator{ref_id_match + ",{1}[0-9]+"};
 
     parser.add_option(options.input_path, 'i', "input_bam",
                       "Input a sorted BAM/SAM file.", seqan3::option_spec::standard,
                       seqan3::input_file_validator{{"sam", "bam"}});
     parser.add_option(options.start, 's', "start",
-                      "The start of the interval to query, in the format chrA:posA."
+                      "The start of the interval to query, in the format chrA,posA."
                       " Note that when start and end are the same, this queries for reads overlapping a point.",
                       seqan3::option_spec::standard,
                       query_validator);
     parser.add_option(options.end, 'e', "end",
-                      "The end of the interval to query, in the format chrB:posB."
+                      "The end of the interval to query, in the format chrB,posB."
                       " Note that when start and end are the same, this queries for reads overlapping a point.",
                       seqan3::option_spec::standard,
                       query_validator);
@@ -104,14 +112,7 @@ int main(int argc, char ** argv)
         return -1;
     }
 
-    using my_fields = seqan3::fields<seqan3::field::id,
-                                     seqan3::field::ref_id,
-                                     seqan3::field::ref_offset,
-                                     seqan3::field::cigar,
-                                     seqan3::field::seq>;
-
     seqan3::debug_stream << "Reading SAM file.\n";
-    seqan3::sam_file_input input{options.input_path, my_fields{}};
 
     seqan3::debug_stream << "Extracting info from reads\n";
 
@@ -126,6 +127,12 @@ int main(int argc, char ** argv)
 
     if (!options.start.empty() && !options.end.empty())
     {
+        using my_fields = seqan3::fields<seqan3::field::id,
+                                         seqan3::field::ref_id,
+                                         seqan3::field::ref_offset,
+                                         seqan3::field::cigar,
+                                         seqan3::field::seq>;
+        seqan3::sam_file_input input{options.input_path, my_fields{}};
         bamit::Position start, end;
         if (parse_overlap_query(start, end, options, input.header().ref_ids()) == -1)
         {
