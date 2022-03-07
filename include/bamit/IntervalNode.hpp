ef6ae1bf609f8ef8d492b34b6bc3db5cad1dca2b
Joshua Kim <joshuak94@zedat.fu-berlin.de>
2022-03-07 13:28:16 +0100

    Fix for gcc versions.

--- include/bamit/IntervalNode.hpp
+++ include/bamit/IntervalNode.hpp
@@ -447,9 +447,7 @@ inline auto get_overlap_records(seqan3::sam_file_input<traits_type, fields_type,
     // when searching for interval 160 - 200, Read 2 will be included in results.
     auto results_list = input | std::views::take_while([file_position](auto & rec) {return file_position != -1;})
                               | std::views::take_while([end](auto & rec) {return std::make_tuple(rec.reference_id().value(), rec.reference_position().value()) < end;})
-                              | std::views::filter([](auto & rec) {return !unmapped(rec);})
-                              | std::views::filter([start](auto & rec) {return std::make_tuple(rec.reference_id().value(),
-                                                                                               get_length((rec).cigar_sequence()) + rec.reference_position().value()) >= start;})
+                              | std::views::filter([start](auto & rec) {return !unmapped(rec) && std::make_tuple(rec.reference_id().value(), get_length((rec).cigar_sequence()) + rec.reference_position().value()) >= start;})
                               | seqan3::views::to<std::vector>;
     if (results_list.empty() && verbose)
     {
